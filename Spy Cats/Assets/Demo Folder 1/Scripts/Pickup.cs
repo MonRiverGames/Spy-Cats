bf493baf8ba298cf6bea8e24e5863d167141209d
Kayla Stein <kstein0119@gmail.com>
2022-09-07 19:45:23 -0400

    Add 'pickup' method
    If an object has the Pickup script applied, when it collides with the Player (with tag 'Player'), the object will get destroyed

---
+++ Spy Cats/Assets/Demo Folder 1/Scripts/Pickup.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
