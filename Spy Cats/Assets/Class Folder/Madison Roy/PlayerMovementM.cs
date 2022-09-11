b048c7ca3efd187ed5aca4e9a1f009fef618f0f6
David <63834304+Itswolfey@users.noreply.github.com>
2022-09-11 13:59:00 -0400

    Update PlayerMovementM.cs

--- Spy Cats/Assets/Class Folder/Madison Roy/PlayerMovementM.cs
+++ Spy Cats/Assets/Class Folder/Madison Roy/PlayerMovementM.cs
@@ -4,15 +4,76 @@ using UnityEngine;
 
 public class PlayerMovementM : MonoBehaviour
 {
+   private float horizontal;
+    private float speed = 8f;
+    private float jumpForce = 10f;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        Flip();
+        Jump();
+
+        if (horizontal > 0.1f || horizontal < -0.1f)
+        {
+            //anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            //anim.SetBool("isWalking", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+    
+    private void Flip()
+    {
+        if(isFacingRight && horizontal < 0 || !isFacingRight && horizontal > 0f)
+        {
+            
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
         
+    }
+    
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+    
+    public void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            //anim.Play("CatJump");
+        }
+        
+        if(Input.GetButtonDown("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            //anim.Play("CatJump");
+        }
     }
 }
