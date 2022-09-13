3993dd83de8b04645e4b56784d7180cdb27017c1
jacksigler <111897835+jacksigler@users.noreply.github.com>
2022-09-13 10:58:56 -0400

    Lost a good amount of progress. Going to be fixing it now. Will be pushing again later today.

---
+++ Spy Cats/Assets/Controller.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    // Tweakable constants
+    [SerializeField] private float playerSpeed = 5.0f;
+    [SerializeField] private float acceleration = 10.0f;
+    [SerializeField] private float jumpVelocity = 5.0f;
+    [SerializeField] private LayerMask jumpMask;
+
+
+    // Components
+    private Rigidbody2D myRigidbody;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
+
+    // Status Variables
+    private bool isJumping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SideMovement();
+
+        if (isJumping && IsGrounded())
+        {
+            isJumping = false;
+            myAnimator.SetBool("isJumping", false);
+        }
+
+        if (IsGrounded() && Input.GetAxisRaw("Vertical") > 0)
+        {
+            Jump();
+        }
+    }
+
+    void SideMovement()
+    {
+        float targetVelocity = Input.GetAxisRaw("Horizontal") * playerSpeed;
+        float xVelocity = Mathf.Lerp(myRigidbody.velocity.x, targetVelocity, acceleration * Time.deltaTime);
+
+        myRigidbody.velocity = new Vector2(xVelocity, myRigidbody.velocity.y);
+        if (IsGrounded())
+        {
+            myAnimator.SetFloat("xSpeed", Mathf.Abs(xVelocity));
+        }
+        else
+        {
+            myAnimator.SetFloat("xSpeed", 0);
+        }
+        if (xVelocity < -.1f)
+        {
+            mySpriteRenderer.flipX = true;
+        } else if (xVelocity > .1f)
+        {
+            mySpriteRenderer.flipX = false;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D groundCheck = Physics2D.Raycast(transform.position, Vector2.down, 2f);
+        return groundCheck.collider != null;
+    }
+
+    void Jump()
+    {
+        isJumping = true;
+        myAnimator.SetBool("isJumping", true);
+        myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpVelocity);
+    }
+    
+}
+
