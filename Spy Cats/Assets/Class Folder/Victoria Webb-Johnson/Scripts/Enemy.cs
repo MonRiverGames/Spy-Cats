6906bf2d195da58b6ccd55922b40d636bc201fb4
nsk0007 <sorenk667@gmail.com>
2022-09-22 22:17:22 -0400

    Deactivated enemy script temporarily

--- Spy Cats/Assets/Class Folder/Victoria Webb-Johnson/Scripts/Enemy.cs
+++ Spy Cats/Assets/Class Folder/Victoria Webb-Johnson/Scripts/Enemy.cs
@@ -13,7 +13,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        target = PlayerManager.instance.player.transform;
+        target = FindObjectOfType<Player>().transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
